70c158c3e6cbb44c466ceef9ccfb09b051c28a3e
Stuart <stuart.pethurst@gmail.com>
2019-03-26 11:03:25 +0000

    Added a hash to the method

--- directory.rb
+++ directory.rb
@@ -1,25 +1,26 @@
 # let's put all students into an array
 
-students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Micheal Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Kruger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
-]
+students_cohort = [
+    {name: "Dr. Hannibal Lecter", cohort: :november},
+    {name: "Darth Vader", cohort: :november},
+    {name: "Nurse Ratched", cohort: :november},
+    {name: "Michael Corleone", cohort: :november},
+    {name: "Alex DeLarge", cohort: :november},
+    {name: "The Wicked Witch of the West", cohort: :november},
+    {name: "Terminator", cohort: :november},
+    {name: "Freddy Krueger", cohort: :november},
+    {name: "The Joker", cohort: :november},
+    {name: "Joffrey Baratheon", cohort: :november},
+    {name: "Norman Bates", cohort: :november}
+  ]
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-def print(names)
-  names.each do |name|
-    puts name
+def print(students_cohort)
+  students_cohort.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 def print_footer(names)
@@ -27,5 +28,5 @@ def print_footer(names)
 end
 #nothing happens until we call the methods
 print_header
-print(students)
-print_footer(students)
+print(students_cohort)
+print_footer(students_cohort)
